c38e1fb13394af3b59d06b359eea2bc7e1826c42
Stephen Mulligan <smulligan@pp.studio>
2023-07-09 20:14:40 +0200

    batch size 16 -> 8

--- train.py
+++ train.py
@@ -192,7 +192,7 @@ if __name__ == "__main__":
     parser.add_argument('--name', type=str, default='test1', help='experiment name')
     parser.add_argument('--iter', type=int, default=50000, help='number of iterations')
     parser.add_argument('--start_iter', type=int, default=0, help='the iteration to start training')
-    parser.add_argument('--batch_size', type=int, default=16, help='mini batch number of images')
+    parser.add_argument('--batch_size', type=int, default=8, help='mini batch number of images')
     parser.add_argument('--im_size', type=int, default=1024, help='image resolution')
     parser.add_argument('--ckpt', type=str, default='None', help='checkpoint weight path if have one')
 
